06847b887e207fbaa716f0597d9b7f19f4e1a739
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-16 22:06:58 -0600

    Implementing health_check.rs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,18 @@
+//! tests/healh_check.rs
 
-#[test]
-fn dummy_test() {    
+#[tokio::test]
+async fn health_check_ok() {
+    spawn_app().await.expect("Failed to spawn app.");
+
+    let client = reqwest::Client::new();
+
+    let response = client.get("http://127.0.0.1:8000/health_check")
+        .send().await.expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> Result<(), std::io::Error> {
+    todo!()
 }
