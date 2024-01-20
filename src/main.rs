46c0e82c67123318445d723605f0d85296ff9d65
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-19 23:20:06 -0600

    Finalizing dynamic port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run("127.0.0.1:8080")?.await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port");
+    run(listener)?.await
 }
