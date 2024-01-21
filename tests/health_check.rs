18cee04910e91ed89e734524ec41ec4e1e8ed5a8
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-21 11:03:14 -0600

    setting up test for subscribe

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,3 +22,20 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=joe&email=joemayo@zero2prod.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
