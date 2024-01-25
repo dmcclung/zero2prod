721bd395ca41972c5d5057841f5ba6c6c0743d07
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-24 22:17:34 -0600

    migrate comment

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // sqlx::migrate!().run(<&your_pool OR &mut your_connection>).await?;
+
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port");
     run(listener)?.await
 }
