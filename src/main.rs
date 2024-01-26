2e65544feaf6b42e0f4a43e34c0dd88a07d37e62
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-26 12:32:35 -0600

    Connection string working

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use sqlx::postgres::Postgres;
 
 use zero2prod::run;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // get the connection setup by building the connection string from a function
     // sqlx::migrate!().run(<&your_pool OR &mut your_connection>).await?;
-    let _pool = Pool::<Postgres>::connect("postgres://").await?;
+    let _pool = Pool::<Postgres>::connect("postgres://admin:admin@localhost:5432/newsletter").await?;
+
+    println!("Created postgres pool");
 
     let listener = TcpListener::bind("127.0.0.1:8080")?;
     Ok(run(listener)?.await?)
