b9b7f9c640b3665ad4aeab33a47f7d364b2e9a72
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-27 17:07:19 -0600

    Modified src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {    
-    let settings = zero2prod::config::get_settings();
+    let config = zero2prod::config::Config::new();
 
-    let pool = PgPoolOptions::new().connect("postgres://admin:admin@localhost:5432/newsletter").await?;
+    let pool = PgPoolOptions::new().connect(&config.db_config.url).await?;
     sqlx::migrate!().run(&pool).await?;
     
-    let addr = format!("[::]:{}", settings.port);
+    let addr = format!("[::]:{}", config.port);
 
     let listener = TcpListener::bind(addr)?;
     Ok(zero2prod::run(listener)?.await?)
