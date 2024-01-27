9e4a0179b0f4d8ca361e8501b1c99682a04b552c
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-26 21:53:59 -0600

    added migrate sqlx call

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::net::TcpListener;
-use sqlx::Pool;
-use sqlx::postgres::Postgres;
+use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // sqlx::migrate!().run(<&your_pool OR &mut your_connection>).await?;
-    let _pool = Pool::<Postgres>::connect("postgres://admin:admin@localhost:5432/newsletter").await?;
-
-    println!("Created postgres pool");
+    let pool = PgPoolOptions::new().connect("postgres://admin:admin@localhost:5432/newsletter").await?;
+    sqlx::migrate!().run(&pool).await?;
 
     let listener = TcpListener::bind("127.0.0.1:8080")?;
     Ok(run(listener)?.await?)
