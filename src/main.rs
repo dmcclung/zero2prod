7624331d198198fa61759c74fd25aa9eb97f36ae
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-27 13:20:50 -0600

    Modified src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
 
-use zero2prod::run;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: use config.rs to pull in the settings for all this
+    let settings = zero2prod::config::Settings{
+        port: 3000,
+    };
+
     let pool = PgPoolOptions::new().connect("postgres://admin:admin@localhost:5432/newsletter").await?;
     sqlx::migrate!().run(&pool).await?;
+    
+    let addr = format!("[::]:{}", settings.port);
 
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    Ok(run(listener)?.await?)
+    let listener = TcpListener::bind(addr)?;
+    Ok(zero2prod::run(listener)?.await?)
 }
