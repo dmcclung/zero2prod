97ddb20c05dea44fbcadd4ded4775d600b8d44a4
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-02 15:45:21 -0600

    setup env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
+use env_logger::Env;
 use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {    
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    
     let config = zero2prod::config::Config::new();
 
     let pool = PgPoolOptions::new().connect(&config.db_config.url).await?;
