ab8b0624703a9b7db575f07a69c62cd3f6b17222
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-11 22:45:57 -0600

    Modified src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,38 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, middleware::Logger, web, App, HttpResponse, HttpServer};
-use sqlx::{Pool, Postgres};
-use uuid::Uuid;
 use chrono::Utc;
-use tracing::Instrument;
+use sqlx::{Pool, Postgres};
+use tracing::{error, info, instrument, Instrument};
+use uuid::Uuid;
 
 pub mod config;
 
-async fn health_check() ->  HttpResponse {
+async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 struct SubscribeData {
     email: String,
-    name: String
+    name: String,
 }
 
-async fn subscribe(form: web::Form<SubscribeData>, pool: web::Data<Pool<Postgres>>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
+#[instrument(
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
-    );
-    
-    let _request_span_guard = request_span.enter();
-    
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database",
-        %request_id
-    );
+    )
+)]
+async fn subscribe(
+    form: web::Form<SubscribeData>,
+    pool: web::Data<Pool<Postgres>>,
+) -> HttpResponse {
+    info!("Adding a new subscriber");
+
+    info!("Saving new subscriber details in the database");
 
     let result = sqlx::query!(
         r#"
@@ -45,28 +45,31 @@ async fn subscribe(form: web::Form<SubscribeData>, pool: web::Data<Pool<Postgres
         Utc::now()
     )
     .execute(pool.get_ref())
-    .instrument(query_span)
+    .instrument(tracing::info_span!("add subscriber query"))
     .await;
 
     match result {
         Ok(_) => {
-            tracing::info!("New subscriber details has been saved");
+            info!("New subscriber details has been saved");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
+            error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
-    }    
+    }
 }
-    
-pub fn run(listener: TcpListener, pool: Pool<Postgres>) -> Result<Server, Box<dyn std::error::Error>> {
+
+pub fn run(
+    listener: TcpListener,
+    pool: Pool<Postgres>,
+) -> Result<Server, Box<dyn std::error::Error>> {
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         let pool = pool.clone();
 
-        App::new()     
-            .wrap(Logger::default())       
+        App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool)
@@ -74,4 +77,4 @@ pub fn run(listener: TcpListener, pool: Pool<Postgres>) -> Result<Server, Box<dy
     .listen(listener)?
     .run();
     Ok(server)
-}   
+}
