ddecd08616bb8773d43113e75b5db6fd6fe63d97
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-11 22:45:57 -0600

    Modified src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
-use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use sqlx::postgres::PgPoolOptions;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
+use std::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {  
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
 
     let config = zero2prod::config::Config::new();
 
     let pool = PgPoolOptions::new().connect(&config.db_config.url).await?;
     sqlx::migrate!().run(&pool).await?;
-    
+
     let addr = format!("[::]:{}", config.port);
 
     let listener = TcpListener::bind(addr)?;
