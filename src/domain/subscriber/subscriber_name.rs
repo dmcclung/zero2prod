1c49bdb238534b38ba76e0229004985b889abdfa
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-18 11:58:31 -0600

    Modified src/domain/subscriber/subscriber_name.rs

--- src/domain/subscriber/subscriber_name.rs
+++ src/domain/subscriber/subscriber_name.rs
@@ -1,18 +1,9 @@
+//! src/domain/subscriber/subscriber_name.rs
+
+use crate::domain::subscriber::SubscriberError;
+
 #[derive(serde::Deserialize, Debug)]
 pub struct SubscriberName(String);
-
-#[derive(Debug)]
-pub enum SubscriberError {
-    ParseError(String),
-}
-
-impl std::fmt::Display for SubscriberError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            SubscriberError::ParseError(e) => write!(f, "Parse Error: {}", e),
-        }
-    }
-}
 
 impl SubscriberName {
     pub fn parse(s: String) -> Result<SubscriberName, SubscriberError> {
