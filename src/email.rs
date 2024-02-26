9a6c117ba944256e0cd40cee436b1d688b32739b
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-25 19:53:23 -0600

    unit tests for email service

--- src/email.rs
+++ src/email.rs
@@ -21,7 +21,16 @@ pub struct SmtpConfig {
 }
 
 impl SmtpConfig {
-    pub fn parse_from_env() -> SmtpConfig {
+    pub fn new(host: String, port: String, user: String, password: String) -> Self {
+        Self {
+            host,
+            port: port.parse::<u16>().unwrap(),
+            user,
+            password,
+        }
+    }
+
+    pub fn parse_from_env() -> Self {
         dotenv::dotenv().ok();
 
         let host = env::var("EMAIL_HOST").unwrap_or("localhost".into());
@@ -29,7 +38,7 @@ impl SmtpConfig {
         let user = env::var("EMAIL_USER").unwrap_or("user".into());
         let password = env::var("EMAIL_PASSWORD").unwrap_or("password".into());
 
-        SmtpConfig {
+        Self {
             host,
             port: port.parse::<u16>().unwrap(),
             user,
@@ -81,13 +90,13 @@ pub struct Email {
 }
 
 pub trait EmailSender {
-    fn send(&self, port: u16, host: &str, creds: Credentials, message: Message) -> Result<()>;
+    fn send(&mut self, port: u16, host: &str, creds: Credentials, message: Message) -> Result<()>;
 }
 
 struct LettreEmailSender;
 
 impl EmailSender for LettreEmailSender {
-    fn send(&self, port: u16, host: &str, creds: Credentials, message: Message) -> Result<()> {
+    fn send(&mut self, port: u16, host: &str, creds: Credentials, message: Message) -> Result<()> {
         let mailer = SmtpTransport::relay(host)?
             .port(port)
             .credentials(creds)
@@ -100,20 +109,20 @@ impl EmailSender for LettreEmailSender {
     }
 }
 
-pub struct EmailService<T: EmailSender> {
+pub struct EmailService<'a, T: EmailSender> {
     config: SmtpConfig,
-    email_sender: T,
+    email_sender: &'a mut T,
 }
 
-impl<T: EmailSender> EmailService<T> {
-    pub fn new(config: SmtpConfig, email_sender: T) -> Self {
+impl<'a, T: EmailSender> EmailService<'a, T> {
+    pub fn new(config: SmtpConfig, email_sender: &'a mut T) -> Self {
         Self {
             config,
             email_sender,
         }
     }
 
-    pub fn send_email(&self, email: Email) -> Result<()> {
+    pub fn send_email(&mut self, email: Email) -> Result<()> {
         let to: Mailbox = email.to.parse()?;
         let from: Mailbox = if email.from.is_empty() {
             DEFAULT_SENDER.parse()?
@@ -149,12 +158,16 @@ impl<T: EmailSender> EmailService<T> {
 
 #[cfg(test)]
 mod tests {
-    use fake::faker::internet::en::{DomainSuffix, Password, Username};
+    use anyhow::Result;
+    use claims::assert_ok;
+    use fake::faker::internet::en::{DomainSuffix, Password, SafeEmail, Username};
+    use fake::faker::lorem::en::Sentence;
     use fake::faker::number::en::NumberWithFormat;
     use fake::Fake;
+    use lettre::Message;
     use std::env::{remove_var, set_var};
 
-    use super::SmtpConfig;
+    use super::{Email, EmailSender, EmailService, SmtpConfig};
 
     #[test]
     fn smtp_config_from_env() {
@@ -169,20 +182,73 @@ mod tests {
         set_var("EMAIL_PASSWORD", password.clone());
 
         let smtp_config = SmtpConfig::parse_from_env();
-        assert_eq!(hostname, smtp_config.host);
-        assert_eq!(username, smtp_config.user);
-        assert_eq!(port.parse::<u16>().unwrap(), smtp_config.port);
-        assert_eq!(password, smtp_config.password);
 
         remove_var("EMAIL_HOST");
         remove_var("EMAIL_USER");
         remove_var("EMAIL_PORT");
         remove_var("EMAIL_PASSWORD");
+
+        assert_eq!(hostname, smtp_config.host);
+        assert_eq!(username, smtp_config.user);
+        assert_eq!(port.parse::<u16>().unwrap(), smtp_config.port);
+        assert_eq!(password, smtp_config.password);
     }
 
     fn generate_hostname() -> String {
         let domain: String = Username().fake();
         let domain_suffix: String = DomainSuffix().fake();
         format!("smtp.{}.{}", domain, domain_suffix)
+    }
+
+    struct MockEmailSender {
+        sent_messages: Vec<Message>,
+    }
+
+    impl MockEmailSender {
+        fn new() -> Self {
+            Self {
+                sent_messages: Vec::new(),
+            }
+        }
+    }
+
+    impl EmailSender for MockEmailSender {
+        fn send(
+            &mut self,
+            _port: u16,
+            _host: &str,
+            _creds: lettre::transport::smtp::authentication::Credentials,
+            message: lettre::Message,
+        ) -> Result<()> {
+            self.sent_messages.push(message);
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn send_valid_email() {
+        let smtp_config = SmtpConfig::new(
+            generate_hostname(),
+            NumberWithFormat("###").fake(),
+            Username().fake(),
+            Password(8..16).fake(),
+        );
+        let email_sender = &mut MockEmailSender::new();
+
+        let email = Email {
+            to: SafeEmail().fake(),
+            from: SafeEmail().fake(),
+            html: "".into(),
+            subject: Sentence(1..5).fake(),
+            reply_to: "".into(),
+            plaintext: Sentence(1..10).fake(),
+        };
+
+        let mut email_service: EmailService<MockEmailSender> =
+            EmailService::new(smtp_config, email_sender);
+        let res = email_service.send_email(email);
+
+        assert_ok!(res);
+        assert_eq!(1, email_sender.sent_messages.len());
     }
 }
