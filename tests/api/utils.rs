b13c0f51609b0ded8ed9b399988fb0b431083455
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-29 15:24:59 -0600

    Modified tests/api/utils.rs

---
+++ tests/api/utils.rs
@@ -0,0 +1,20 @@
+//! tests/api/utils.rs
+
+use std::net::TcpListener;
+
+use sqlx::postgres::PgPoolOptions;
+
+pub async fn spawn_app() -> Result<String, sqlx::Error> {
+    let config = zero2prod::config::Config::new();
+    let pool = PgPoolOptions::new().connect(&config.db_config.url).await?;
+    sqlx::migrate!().run(&pool).await?;
+    sqlx::query!("DELETE FROM subscriptions")
+        .execute(&pool)
+        .await?;
+
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener, pool).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    Ok(format!("http://127.0.0.1:{}", port))
+}
