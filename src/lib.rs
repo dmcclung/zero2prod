f0501dd77cd584940d7a050a332348a5f4fba329
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-02-29 22:40:10 -0600

    Modified src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpResponse, HttpServer};
+use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use domain::subscriber::{NewSubscriber, SubscriberEmail, SubscriberError, SubscriberName};
 use serde::Deserialize;
 use sqlx::{Pool, Postgres};
 use tracing::{error, info, instrument, Instrument};
 use uuid::Uuid;
+use anyhow::Result;
 
 pub mod config;
 pub mod domain;
 pub mod email;
+pub mod app;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -73,23 +73,4 @@ async fn subscribe(
             HttpResponse::InternalServerError().finish()
         }
     }
-}
-
-pub fn run(
-    listener: TcpListener,
-    pool: Pool<Postgres>,
-) -> Result<Server, Box<dyn std::error::Error>> {
-    let pool = web::Data::new(pool);
-    let server = HttpServer::new(move || {
-        let pool = pool.clone();
-
-        App::new()
-            .wrap(Logger::default())
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .app_data(pool)
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
 }
