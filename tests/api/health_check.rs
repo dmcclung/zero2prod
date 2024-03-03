d3d33ec678459827cdbbdc1f0a9dbedf5e458c1a
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-03-02 22:36:24 -0600

    Updated tests/api/health_check.rs

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,15 +1,19 @@
 //! tests/api/healh_check.rs
 
+use claims::assert_ok;
+
 use crate::utils::spawn_app;
 
 #[tokio::test]
 async fn health_check_ok() {
-    let app_address = spawn_app().await.unwrap();
+    let test_app = spawn_app().await.unwrap();
+
+    assert_ok!(test_app.reset_subscriptions().await);
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &app_address))
+        .get(&format!("{}/health_check", test_app.address()))
         .send()
         .await
         .expect("Failed to execute request.");
