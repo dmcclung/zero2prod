907a4374d3afcfd2174172817396a67785c65e59
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-03-20 17:00:43 -0500

    Modified src/routes/confirm.rs

--- src/routes/confirm.rs
+++ src/routes/confirm.rs
@@ -2,5 +2,8 @@
 use actix_web::HttpResponse;
 
 pub async fn confirm() -> HttpResponse {
+    // TODO: Get token and lookup from database
+    // if token is in the database, look up user and set status to confirm
+    // delete token from db
     HttpResponse::Ok().finish()
 }
