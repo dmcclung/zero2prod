ecd60f7a608de53dc52ddd0c771912cb53982f29
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-03-21 23:44:29 -0500

    Modified tests/api/confirm.rs

--- tests/api/confirm.rs
+++ tests/api/confirm.rs
@@ -1,11 +1,14 @@
 use crate::test_app::spawn;
+use fake::{uuid::UUIDv4, Fake};
 
 #[tokio::test]
 async fn confirm_returns_200_with_valid_token() {
     let test_app = spawn().await.unwrap();
 
+    let uuid: String = UUIDv4.fake();
+
     let response = test_app
-        .confirm_subscription()
+        .confirm_subscription(&uuid)
         .await
         .expect("Failed to execute request.");
 
