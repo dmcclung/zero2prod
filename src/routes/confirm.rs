841e1d3bd332a64ee82853b13636fc107afaa5c2
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-03-22 16:34:05 -0500

    improving confirm handler

--- src/routes/confirm.rs
+++ src/routes/confirm.rs
@@ -1,17 +1,54 @@
 //! src/routes/confirm.rs
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
-use tracing::info;
+use sqlx::Pool;
+use sqlx::Postgres;
+use tracing::instrument;
+use tracing::Instrument;
+use tracing::{error, info};
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
 pub struct ConfirmRequest {
     token: String,
 }
 
-pub async fn confirm(info: web::Query<ConfirmRequest>) -> HttpResponse {
-    // TODO: Get token and lookup from database
+#[instrument(
+    skip(pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+    )
+)]
+pub async fn confirm(
+    info: web::Query<ConfirmRequest>,
+    pool: web::Data<Pool<Postgres>>,
+) -> HttpResponse {
     info!("Confirming subscription {}", info.token);
-    // if token is in the database, look up user and set status to confirm
-    // delete token from db
+
+    let result = sqlx::query!(
+        r#"
+        SELECT subscription_token, subscriber_id FROM subscription_tokens
+        WHERE subscription_token = $1        
+        "#,
+        info.token,
+    )
+    .fetch_one(pool.get_ref())
+    .instrument(tracing::info_span!("confirm subscription query"))
+    .await;
+
+    match result {
+        Ok(record) => {
+            info!("Subscription confirmed {}", record.subscriber_id);
+            // TODO: if token is in the database, look up user and set status to confirm
+
+            // TODO: delete token from db
+        }
+        Err(e) => {
+            error!("Error fetching confirmation token {}", e);
+            // return HttpResponse::BadRequest if token not found
+            // return InternalServerError if something else
+        }
+    }
+
     HttpResponse::Ok().finish()
 }
