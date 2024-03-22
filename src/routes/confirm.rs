a041de14e7b51e84761be89f7b950d050b21f939
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-03-21 23:44:28 -0500

    Modified src/routes/confirm.rs

--- src/routes/confirm.rs
+++ src/routes/confirm.rs
@@ -1,8 +1,16 @@
-//! src/routes/health_check.rs
-use actix_web::HttpResponse;
+//! src/routes/confirm.rs
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
+use tracing::info;
 
-pub async fn confirm() -> HttpResponse {
+#[derive(Debug, Deserialize)]
+pub struct ConfirmRequest {
+    token: String,
+}
+
+pub async fn confirm(info: web::Query<ConfirmRequest>) -> HttpResponse {
     // TODO: Get token and lookup from database
+    info!("Confirming subscription {}", info.token);
     // if token is in the database, look up user and set status to confirm
     // delete token from db
     HttpResponse::Ok().finish()
