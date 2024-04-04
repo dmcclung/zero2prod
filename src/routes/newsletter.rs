2142be8dfd4cc92dc31952ccc1ca83c13074f4bb
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-04-03 22:50:05 -0500

    improving newsletter route

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,17 +1,46 @@
 //! src/routes/newsletter.rs
 
+use std::sync::Arc;
+
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
-use tracing::info;
+use sqlx::{Pool, Postgres};
+use tracing::{info, instrument, Instrument};
+use uuid::Uuid;
+
+use crate::{domain::subscriber::SubscriberError, email::EmailService};
 
 #[derive(Deserialize)]
 pub struct NewsletterJson {
     pub newsletter: String,
 }
 
+#[instrument(
+    skip(json, pool, email_service),
+    fields(
+        request_id = %Uuid::new_v4()
+    )
+)]
 pub async fn publish_newsletter(
     json: web::Json<NewsletterJson>,
-) -> HttpResponse {
+    pool: web::Data<Pool<Postgres>>,
+    email_service: web::Data<Arc<dyn EmailService + Send + Sync>>,
+) -> Result<HttpResponse, actix_web::Error> {
     info!("{}", json.0.newsletter);
-    HttpResponse::Ok().finish()
+    let confirmed_emails = sqlx::query!(
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#
+    )
+    .fetch_all(pool.get_ref())
+    .instrument(tracing::info_span!("get confirmed emails query"))
+    .await
+    .map_err(SubscriberError::DatabaseError)?;
+
+    info!("Confirmed email addresses: {}", confirmed_emails.len());
+
+    // send them an email
+    Ok(HttpResponse::Ok().finish())
 }
