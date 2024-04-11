2f8a856d619afb5d77dba88a229ebd8633d69cbf
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-04-10 23:31:26 -0500

    feat(test): improved message with newsletter publish 400 bad request tests

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -28,13 +28,28 @@ async fn publish_newsletter_returns_400_with_bad_html_text() {
     let html = String::from(format!("<p>{}</p>", text));
 
     let test_cases = [
-        (None, None, None),
-        (None, None, Some(subject.clone())),
-        (None, Some(text.clone()), Some(subject.clone())),
-        (None, Some(text.clone()), None),
-        (Some(html.clone()), None, Some(subject)),
-        (Some(html.clone()), None, None),
-        (Some(html), Some(text), None),
+        (None, None, None, "Missing all fields"),
+        (None, None, Some(subject.clone()), "Missing body"),
+        (
+            None,
+            Some(text.clone()),
+            Some(subject.clone()),
+            "Missing html body",
+        ),
+        (
+            None,
+            Some(text.clone()),
+            None,
+            "Missing subject and html body",
+        ),
+        (Some(html.clone()), None, Some(subject), "Missing text body"),
+        (
+            Some(html.clone()),
+            None,
+            None,
+            "Missing text body and subject",
+        ),
+        (Some(html), Some(text), None, "Missing subject"),
     ];
 
     let test_app = spawn().await.unwrap();
@@ -44,7 +59,12 @@ async fn publish_newsletter_returns_400_with_bad_html_text() {
             .publish_newsletter(test_case.0, test_case.1, test_case.2)
             .await
             .expect("Failed to post subscription");
-        assert_eq!(400, response.status().as_u16());
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail with 400 Bad Request, {}",
+            test_case.3
+        );
     }
 }
 
