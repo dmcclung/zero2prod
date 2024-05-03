7e16bf33b521cedabb30e39ae02d72fe26961f67
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-03 04:09:37 -0500

    returning rendered home template

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,6 +1,11 @@
 //! src/routes/home.rs
 use actix_web::HttpResponse;
+use askama::Template;
+
+use crate::templates::HomeTemplate;
 
 pub async fn home() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    let home_template = HomeTemplate{};
+    let home_rendered = home_template.render().unwrap();
+    HttpResponse::Ok().body(home_rendered)
 }
