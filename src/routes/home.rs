a301deec050e17095b62df64a1d555bfe90c5288
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-04 04:09:37 -0500

    setting content_type to html in home response

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -3,9 +3,12 @@ use actix_web::HttpResponse;
 use askama::Template;
 
 use crate::templates::HomeTemplate;
+use actix_web::http::header::ContentType;
 
 pub async fn home() -> HttpResponse {
-    let home_template = HomeTemplate{};
+    let home_template = HomeTemplate {};
     let home_rendered = home_template.render().unwrap();
-    HttpResponse::Ok().body(home_rendered)
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(home_rendered)
 }
