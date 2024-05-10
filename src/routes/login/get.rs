2436f7026a46296e8c9b92572ca94207d6c0c830
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-09 22:28:59 -0500

    writing login template in login_form

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,5 +1,13 @@
+use crate::templates::LoginTemplate;
+use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
+use askama::Template;
 
 pub async fn login_form() -> HttpResponse {
+    let login_template = LoginTemplate {};
+    let login_rendered = login_template.render().unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(login_rendered);
     HttpResponse::Ok().finish()
 }
