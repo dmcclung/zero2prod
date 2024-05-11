9ed6438187681cb001007040067332065687dc71
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-10 22:26:12 -0500

    setting up loginform data in post handler

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,12 @@
-use actix_web::{http::header, HttpResponse};
+use actix_web::{http::header, web, HttpResponse};
 
-pub async fn login() -> HttpResponse {
+#[derive(serde::Deserialize)] 
+pub struct LoginFormData {
+    _username: String,
+    _password: String, 
+}
+
+pub async fn login(_form: web::Form<LoginFormData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((header::LOCATION, "/"))
         .finish()
