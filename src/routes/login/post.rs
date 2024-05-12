69e742140924725aa105096b28101cfa4fe9217a
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-12 16:32:52 -0500

    achanged LoginFormData to use a secrecy secret field

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,9 +1,10 @@
 use actix_web::{http::header, web, HttpResponse};
+use secrecy::Secret;
 
 #[derive(serde::Deserialize)]
 pub struct LoginFormData {
     _username: String,
-    _password: String,
+    _password: Secret<String>,
 }
 
 pub async fn login(_form: web::Form<LoginFormData>) -> HttpResponse {
