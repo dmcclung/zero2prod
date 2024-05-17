c70cb03afea5c519a8a8be52abc9ebdef5480f83
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-05-16 20:13:09 -0500

    making progress on post login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -2,10 +2,12 @@ use actix_web::{http::header, web, HttpResponse};
 use secrecy::Secret;
 use sqlx::{Pool, Postgres};
 
+use crate::auth::{validate_credentials, Credentials};
+
 #[derive(serde::Deserialize)]
 pub struct LoginFormData {
-    _username: String,
-    _password: Secret<String>,
+    username: String,
+    password: Secret<String>,
 }
 
 #[tracing::instrument(
@@ -15,8 +17,17 @@ pub struct LoginFormData {
 pub async fn login(
     form: web::Form<LoginFormData>,
     pool: web::Data<Pool<Postgres>>,
-) -> HttpResponse {
-    HttpResponse::SeeOther()
+) -> Result<HttpResponse, actix_web::Error> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentials(credentials, pool.get_ref()).await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(user_id));
+
+    Ok(HttpResponse::SeeOther()
         .insert_header((header::LOCATION, "/"))
-        .finish()
+        .finish())
 }
